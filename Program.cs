873b931d67fee1f7e76b98e82a133f36b1dca1dc
Vasiok <vasiok26@gmail.com>
2016-05-27 12:53:34 +0300

    Create project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Екуqwert");
+            Console.ReadLine();
+        }
+    }
+}
